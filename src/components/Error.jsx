7dd00df398341648555df756ec9e2e852a037d8b
Sergio Sebastian burgos <sebamayorde18@gmail.com>
2022-10-12 13:45:55 -0300

    react-router dom crm finished

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
-
-function Error({ children }) {
+function Error({children}) {
   return (
-    <div className='text-center py-4 bg-red-600 text-white font-bold uppercase'>
+    <div className="text-center my-4 bg-red-600 text-white font-bold p-3 uppercase">
         {children}
     </div>
   )
